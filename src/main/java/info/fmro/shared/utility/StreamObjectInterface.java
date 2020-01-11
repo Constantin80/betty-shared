fb95b41f30f042dd51393b3da54aac3d14b9b269
Constantin <alex@fmro.info>
2020-01-11 22:15:17 +0000

    .1

--- src/main/java/info/fmro/shared/utility/StreamObjectInterface.java
+++ src/main/java/info/fmro/shared/utility/StreamObjectInterface.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public interface StreamObjectInterface
         extends Serializable {
+    StreamObjectInterface getCopy();
+
     int runAfterReceive();
 
     void runBeforeSend();
